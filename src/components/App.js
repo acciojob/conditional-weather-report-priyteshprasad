4d5d977d25fff18b377723a925dccd1bfa481d0b
Priytesh Prasad <priyteshprasd@gmail.com>
2024-05-19 12:27:25 +0530

    sol1

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,37 @@
+import React, { useEffect, useState } from "react";
+import "./../styles/App.css";
 
-import React from "react";
-import './../styles/App.css';
+let weatherInput = { temperature: 25, conditions: "Sunny" };
+
+const WeatherDisplay = ({ weatherData }) => {
+  console.log(weatherData);
+  return (
+    <>
+      <p
+        style={
+          weatherData.temperature > 20 ? { color: "red" } : { color: "blue" }
+        }
+      >
+        Temperature: {weatherData.temperature}
+      </p>
+      <p>Conditions: {weatherData.conditions}</p>
+      <span>K</span>
+    </>
+  );
+};
 
 const App = () => {
+  const [data, setData] = useState(weatherInput);
+  console.log("line25", data);
+  useEffect(() => {
+    setData(weatherInput);
+  }, []);
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <WeatherDisplay weatherData={data} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
